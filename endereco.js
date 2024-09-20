e42d8771bb2b5c1f74183f30369d392db364f8d1
Pamela <p.silva39@alunoescolassociais.org.br>
2024-09-20 07:30:56 -0300

    coloca o valor da API dentro do campo do formulario

--- endereco.js
+++ endereco.js
@@ -12,7 +12,7 @@ const limparFormulario = () =>{
     document.getElementById('Estado').value = '';
     document.getElementById('Complemento').value = '';
     document.getElementById('CEP').value = '';
-    document.getElementById('Número').value = '';
+    document.getElementById('Nú').value = '';
   
 
 }
@@ -21,3 +21,12 @@ const limparFormulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho do CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+//Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('Rua').value = endereco.Rua; // Coloca o valor de logradouro da API dentro do campo logradouro do formulário
+    document.getElementById('Bairro').value = endereco.Bairro;
+    document.getElementById('Localidade').value = endereco.Localidade;
+    document.getElementById('Uf').value = endereco.Uf;
+}
+
