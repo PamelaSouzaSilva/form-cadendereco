39a58d3be525acdf536711774ccc1730af9955ae
Pamela <p.silva39@alunoescolassociais.org.br>
2024-09-26 12:29:11 -0300

    correção do arquivo Java Script

--- endereco.js
+++ endereco.js
@@ -18,7 +18,7 @@ const limparFormulario = () =>{
 }
 
 // Verifica se o CEP é válido
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const eNumero = (numero) => /^[0-9]+$/.test(numero); // Expressão Regular
 // Verifica o tamanho do CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
@@ -30,3 +30,24 @@ const preencherFormulario = (endereco) =>{
     document.getElementById('Estado').value = endereco.Estado;
 }
 
+// Fução para consumo de API ViaCEP
+const pesquisarCEP = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+
+        if(addres.hasOwnProperty('erro')){
+            alert('CEP não encontrado');
+        }else{
+            preencherFormulario(addres);
+        }  
+
+    }else{
+        alert('CEP incorreto');
+    }
+ }
+
+ // Executa a ação de preenchimento do formulário ao deixar o campo do CEP 
+ document.getElementById('CEP').addEventListener('focusout', pesquisarCEP);
